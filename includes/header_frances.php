f27587988b5d205e7af326f4f0e1f09fbccc894b
Hellynger St Rose <you@example.com>
2022-05-22 21:21:22 -0500

    ya funciona los 3 idiomas y el link de borrar cookie

--- includes/header_frances.php
+++ includes/header_frances.php
@@ -19,12 +19,12 @@ function encabezado()
     <section class="container"> 
       
         <div class="logo"> 
-           <a href="./inicio.php">
+           <a href="./inicio_frances.php">
         <img class="horoscopo" src="imagenes/horoscopo.png" alt="zodiac"><span> </span></a>
        <h1>Votre horoscope</h1>
         </div>
         <nav class="menu">
-          <a href="inicio.php">Début</a> 
+          <a href="inicio_frances.php">Début</a> 
           <a href="https://doc.clickup.com/31045686/d/h/xke1p-283/8ec2db1806fa6a4" target="_blank" >Signes du zodiaque</a>
           <a href="https://github.com/FernandoCutire/zodiaco-app/tree/master" target="_blank">À propos de</a>
           
