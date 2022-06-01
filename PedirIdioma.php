cd7ed08d9c0be51d1ea81929e6f111989b3ad952
Hellynger St Rose <you@example.com>
2022-06-01 14:36:15 -0500

    se arreglo el contador para que se restablezca y idiomas horizontal

--- PedirIdioma.php
+++ PedirIdioma.php
@@ -17,10 +17,11 @@ function idiomas()
     <section class="section">
     <h2 class="title-form">Escoge tu idioma de preferencia</h2>
     <div class="banderas">
+
     <a href="javascript:enviarIdioma('es');"><img src="imagenes/españa.png" alt="españa" style="width: 130px; height: 130px;"></a>
-    <br>
+    
     <a href="javascript:enviarIdioma('en');"><img src="imagenes/ingles.png" alt="ingles" style="width: 150px; height: 150px;"></a>
-    <br>
+    
     <a href="javascript:enviarIdioma('fr');"><img src="imagenes/francia.png" alt="francia" style="width: 150px; height: 140px;"></a>
     
     </div>
